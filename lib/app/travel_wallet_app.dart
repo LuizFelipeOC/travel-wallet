6ef01e486d6f304a61f7b9a2d710f3bdee40b807
_costa <luizfelipeeoliveiraac@gmail.com>
2026-07-14 19:27:43 -0400

    first commit

---
+++ lib/app/travel_wallet_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TravelWalletApp extends StatefulWidget {
+  const TravelWalletApp({super.key});
+
+  @override
+  State<TravelWalletApp> createState() => _TravelWalletAppState();
+}
+
+class _TravelWalletAppState extends State<TravelWalletApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:travel_wallet/app/travel_wallet_app.dart';
+
+void main() {
+  runApp(const TravelWalletApp());
+}
