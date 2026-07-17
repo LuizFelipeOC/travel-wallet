7f0f2863d91d78aa8ebd49dac64faa6633824be4
_costa <luizfelipeeoliveiraac@gmail.com>
2026-07-16 21:27:03 -0400

    feat: add primaryGradient to AppColors for enhanced theming

--- lib/app/core/constants/app_colors.dart
+++ lib/app/core/constants/app_colors.dart
@@ -1,5 +1,11 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 final class AppColors {
   static const Color primaryColor = Color(0xFF163592);
+
+  static LinearGradient get primaryGradient => const LinearGradient(
+    colors: [Color(0xFF163592), Color(0xFF1E4D9B)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
 }
