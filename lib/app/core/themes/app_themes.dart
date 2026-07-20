a7a52dcb388e264b7a0169b3e2689cf52cba5d3f
_costa <luizfelipeeoliveiraac@gmail.com>
2026-07-20 18:58:43 -0400

    feat: update theme colors and improve text styles for light and dark themes

--- lib/app/core/themes/app_themes.dart
+++ lib/app/core/themes/app_themes.dart
@@ -5,7 +5,7 @@ import '../constants/constants.dart';
 final class AppThemes {
   static ThemeData get lightTheme => ThemeData(
     brightness: Brightness.light,
-    primaryColor: AppColors.primaryColor,
+    primaryColor: AppColors.amber300,
     scaffoldBackgroundColor: Colors.white,
     fontFamily: 'Inter18',
     useMaterial3: false,
@@ -15,10 +15,22 @@ final class AppThemes {
     textButtonTheme: textButtonTheme(),
   );
 
+  static ThemeData get darkTheme => ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: AppColors.amber300,
+    scaffoldBackgroundColor: AppColors.neutral800,
+    fontFamily: 'Inter18',
+    useMaterial3: false,
+    textTheme: darkTextTheme(),
+    elevatedButtonTheme: elevatedButtonTheme(),
+    inputDecorationTheme: inputDecorationTheme(),
+    textButtonTheme: textButtonTheme(),
+  );
+
   static TextButtonThemeData textButtonTheme() {
     return TextButtonThemeData(
       style: TextButton.styleFrom(
-        foregroundColor: AppColors.primaryColor,
+        foregroundColor: AppColors.amber300,
         textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
       ),
     );
@@ -30,7 +42,7 @@ final class AppThemes {
       fillColor: Colors.white,
       contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12),
       labelStyle: const TextStyle(color: Colors.black87),
-      suffixIconColor: AppColors.primaryColor,
+      suffixIconColor: AppColors.amber300,
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(12),
         borderSide: BorderSide(color: Colors.grey.shade300),
@@ -41,7 +53,7 @@ final class AppThemes {
       ),
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(12),
-        borderSide: BorderSide(color: AppColors.primaryColor, width: 2),
+        borderSide: BorderSide(color: AppColors.amber300, width: 2),
       ),
     );
   }
@@ -49,7 +61,7 @@ final class AppThemes {
   static ElevatedButtonThemeData elevatedButtonTheme() {
     return ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        backgroundColor: AppColors.primaryColor,
+        backgroundColor: AppColors.amber300,
         foregroundColor: Colors.white,
         textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
@@ -58,18 +70,39 @@ final class AppThemes {
   }
 
   static TextTheme textTheme() {
-    return const TextTheme(
+    return TextTheme(
       headlineSmall: TextStyle(
         fontSize: 24,
-        fontWeight: .bold,
-        color: Colors.black,
+        fontWeight: FontWeight.bold,
+        color: AppColors.neutral800,
         height: 1.3,
         letterSpacing: 0.5,
         wordSpacing: 0.5,
       ),
       bodyMedium: TextStyle(
         fontSize: 16,
-        color: Colors.black,
+        color: AppColors.neutral800,
+        height: 1.3,
+        letterSpacing: 0.5,
+        wordSpacing: 0.5,
+        fontWeight: FontWeight.normal,
+      ),
+    );
+  }
+
+  static TextTheme darkTextTheme() {
+    return TextTheme(
+      headlineSmall: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color: AppColors.slate50,
+        height: 1.3,
+        letterSpacing: 0.5,
+        wordSpacing: 0.5,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 16,
+        color: AppColors.slate200,
         height: 1.3,
         letterSpacing: 0.5,
         wordSpacing: 0.5,
