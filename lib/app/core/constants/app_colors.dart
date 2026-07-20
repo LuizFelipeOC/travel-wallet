72d678602c62137b46def2bb7b52babd61a63ed9
_costa <luizfelipeeoliveiraac@gmail.com>
2026-07-20 18:58:48 -0400

    feat: add new color constants for improved UI design

--- lib/app/core/constants/app_colors.dart
+++ lib/app/core/constants/app_colors.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 final class AppColors {
-  static const Color primaryColor = Color(0xFF163592);
+  static const Color amber300 = Color(0xFFfbbf24);
+
+  static const Color slate50 = Color(0xFFf8fafc);
+  static const Color slate100 = Color(0xFFf1f5f9);
+  static const Color slate200 = Color(0xFFe2e8f0);
+
+  static const Color neutral800 = Color(0xFF262626);
 
   static LinearGradient get primaryGradient => const LinearGradient(
     colors: [Color(0xFF163592), Color(0xFF1E4D9B)],
