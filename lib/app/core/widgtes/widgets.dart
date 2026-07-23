7b7a1ff53870d2c6b4f6d8ee2d87377c7c207479
_costa <luizfelipeeoliveiraac@gmail.com>
2026-07-22 20:11:27 -0400

    refactor(widgets): remove unused widgets export file

--- lib/app/core/widgtes/widgets.dart
+++
@@ -1,5 +0,0 @@
-export 'app_svg/app_svg.dart';
-export 'fade_in/fade_in.dart';
-export 'custom_buttons/cusom_button.dart';
-export "app_bar/custom_silver_app_bar.dart";
-export "app_bar/custom_silver_home_bar.dart";
